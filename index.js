04c3f8dfe554dac8b609051f5cfbe8be1a3bf259
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-10-23 01:01:20 +0300

    fix: esm error

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
-const { copyFolder } = require('copy-folder-util');
 const path = require('path');
 
-const scriptDir = __dirname;
-const projectName = process.argv[2];
-const projectDir = projectName ? path.join(process.cwd(), projectName) : process.cwd();
+async function main() {
+    const { copyFolder } = await import('copy-folder-util');
+    const scriptDir = __dirname;
+    const projectName = process.argv[2];
+    const projectDir = projectName ? path.join(process.cwd(), projectName) : process.cwd();
 
-const templateDir = path.join(scriptDir, 'template');
+    const templateDir = path.join(scriptDir, 'template');
 
-console.log(`Creating instant-docs at ${projectDir}`);
-copyFolder(templateDir, projectDir);
-console.log('Completed');
+    console.log(`Creating instant-docs at ${projectDir}`);
+    copyFolder.cp(templateDir, projectDir);
+    console.log('Completed');
+}
 
+main();
