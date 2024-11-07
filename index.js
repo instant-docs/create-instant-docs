7b44d4e43fa2920a42495d6ff583e18faf1cb8c4
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-11-07 23:25:56 +0300

    feat: cd messages

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ async function main() {
     const { copyFolder } = await import('copy-folder-util');
     const scriptDir = __dirname;
     const projectName = process.argv[2];
-    const projectDir = projectName ? path.join(process.cwd(), projectName) : process.cwd();
+    if(!projectName ) {
+        console.warn('Creating project in the current directory');
+    }
+    const projectDir = path.join(process.cwd(), projectName || '.');
 
     const templateDir = path.join(scriptDir, 'template');
 
     console.log(`Creating instant-docs at ${projectDir}`);
     copyFolder.cp(templateDir, projectDir);
     console.log('Completed');
-    console.log('First install dependencies with your favourite package manager');
+    if (projectName) {
+        console.log('Please first navigate to the directory');
+        console.log(`e.g. cd ${projectName}`);
+    }
+    console.log('Install dependencies with your favourite package manager');
     console.log('e.g. npm install')
     console.log('Then start server');
     console.log('e.g. npm start')
